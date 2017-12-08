02c62888237e9b256d2b72db9ab951ef377084ef
Nikita <Nkmelnikov@hotmail.com>
2017-12-08 15:50:04 +0300

    responsive adaptive header

--- resources/views/layouts/parts/header.blade.php
+++ resources/views/layouts/parts/header.blade.php
@@ -4,12 +4,12 @@
   </div>
   <div class="col menu-box">
     <ul class="row menu-list">
-      <li><a href="#"></a>@lang('header.item1')</li>
-      <li><a href="#"></a>@lang('header.item2')</li>
-      <li><a href="#"></a>@lang('header.item3')</li>
-      <li><a href="#"></a>@lang('header.item4')</li>
-      <li><a href="#"></a>@lang('header.item5')</li>
-      <li><a href="#"></a>@lang('header.item6')</li>
+      <li><a href="#">@lang('header.item1')</a></li>
+      <li><a href="#">@lang('header.item2')</a></li>
+      <li><a href="#">@lang('header.item3')</a></li>
+      <li><a href="#">@lang('header.item4')</a></li>
+      <li><a href="#">@lang('header.item5')</a></li>
+      <li><a href="#">@lang('header.item6')</a></li>
     </ul>
   </div>
   <div class="col-1 lang-box">
@@ -21,4 +21,39 @@
       @endif
     @endforeach
   </div>
+  <div class="col burger-box">
+    <a href="#" class="btn btn-md">
+      <span class="glyphicon glyphicon-menu-hamburger"></span>
+    </a>
+  </div>
 </div>
+<div class="mob-menu">
+  <ul class="mob-menu-list ">
+    <li><a href="#">@lang('header.item1')</a></li>
+    <li><a href="#">@lang('header.item2')</a></li>
+    <li><a href="#">@lang('header.item3')</a></li>
+    <li><a href="#">@lang('header.item4')</a></li>
+    <li><a href="#">@lang('header.item5')</a></li>
+    <li><a href="#">@lang('header.item6')</a></li>
+    <br>
+    <li class="mob-lang">@foreach (Config::get('languages') as $lang => $language)
+        @if ($lang != App::getLocale())
+          <a class="lang-switcher" href="{{ route('lang.switch', $lang) }}">
+            {{ $lang == 'ru' ? 'rus' : 'eng' }}
+          </a>
+        @endif
+      @endforeach
+    </li>
+  </ul>
+</div>
+@push('scripts')
+  <script>
+		$(document).ready(function () {
+
+			$('.burger-box a').on('click', function () {
+				$('.mob-menu').toggleClass('active');
+			});
+
+		});
+  </script>
+@endpush
